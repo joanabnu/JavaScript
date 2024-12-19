3dbd8a474686a2a68243eccf0669d691ed32d701
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2024-12-19 17:58:02 -0300

    Verificador de idade

--- texte/texte.js
+++
@@ -1,8 +0,0 @@
-var hora = new Date();
-
-// Horário da França em português
-var horarioFrancaPt = hora.toLocaleString("pt-BR", { timeZone: "Europe/Paris" });
-
-
-console.log(`Horário na França (pt-BR): ${horarioFrancaPt}`);
-
