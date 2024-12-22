7fd4dbe804f7de0840694906d86fe2cab05d969e
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2024-12-22 18:32:31 -0300

    Vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 9, 0, 3]
+num.push(7)
+num.sort()
+console.log(`Nossa vetor ${num}`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O VETOR TEM ${num.length} posiçoes`)
+
