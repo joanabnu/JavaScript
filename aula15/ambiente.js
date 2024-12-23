2ef46127e94b1c886ab6d90040daaf41db67ac92
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2024-12-22 21:41:04 -0300

    vetores

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,4 +4,10 @@ num.sort()
 console.log(`Nossa vetor ${num}`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 console.log(`O VETOR TEM ${num.length} posiçoes`)
-
+let pos = num.indexOf(4)
+if (pos == -1){
+    console.log(`nao foi emcontrado`)
+}else{
+    console.log(`${pos}`)
+}
+console.log(`O valor 8 esta na posição ${pos}`)
